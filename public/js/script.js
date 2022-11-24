0347eabc92ed59f81dd15fc518622d867be4536b
kuroimi <mikakakomatsu@gmail.com>
2022-11-24 16:28:07 +1100

    initial

---
+++ public/js/script.js
@@ -0,0 +1,19 @@
+const clickMe = () => {
+
+    alert("Thanks for clicking me. Hope you have a nice day!")
+
+}
+
+
+
+$(document).ready(function(){
+
+    $('.materialboxed').materialbox();
+
+    $('#clickMeButton').click(()=>{
+
+        clickMe();
+
+    })
+
+  });
