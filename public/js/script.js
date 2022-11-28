f14ac4a72d58e317f0f9933429d2f9575bf68780
kuroimi <mikakakomatsu@gmail.com>
2022-11-29 07:41:18 +1100

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,19 +1,41 @@
-const clickMe = () => {
+.navbar-size {​
 
-    alert("Thanks for clicking me. Hope you have a nice day!")
+    width: 95%;​
 
-}
+    margin: 0 auto;​
 
+}​
 
+.user-image-size {​
 
-$(document).ready(function(){
+    height: 56px;​
 
-    $('.materialboxed').materialbox();
+    margin: 4px 0;​
 
-    $('#clickMeButton').click(()=>{
+}​
 
-        clickMe();
+.main-body {​
 
-    })
+    background-color: #F28487;​
 
-  });
+    color: white;​
+
+    height: calc(100vh - 64px);​
+
+}​
+
+.materialboxed-image {​
+
+    margin: 0 auto;​
+
+    height: 400px;​
+
+}​
+
+.click-me-button {​
+
+    background-color: #EE6E73;​
+
+    margin-top: 50px;​
+
+}​
